c93be9de14efe74d55a7c557ba20245ef0bb0ca7
Ben <benferree3@gmail.com>
2017-11-26 17:52:17 -0700

    Does not open eight instance when the menu is opened

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 if (CLIENT) then
-print( "opened" )
+print( "opened and key_ran set to 1" )
+key_ran = 1
 hook.Add( "Think", "Open Menu", function()
     if( input.IsKeyDown( KEY_INSERT ) ) then
         if key_ran == 1 then
@@ -8,8 +9,12 @@ hook.Add( "Think", "Open Menu", function()
     end
         end
 end )
-        local function HackDerma()
+hook.Add ("Think", "Close Menu", function ()
+    if( input.IsKeyDown( KEY_DELETE ) ) then
         key_ran = 1
+    end
+end)
+        local function HackDerma()
         local midW, midH = ScrW() / 2
         local HackDerma = vgui.Create("DFrame")
         HackDerma:SetSize(500, 450)
@@ -18,4 +23,5 @@ end )
         HackDerma:MakePopup()
       end
       concommand.Add( "open", HackDerma )
-en
+end
+
