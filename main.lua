f516440511a429ba6963445bf75bebe22f45b3b4
Ben <benferree3@gmail.com>
2017-11-27 20:13:20 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,18 +9,29 @@ hook.Add( "Think", "Open Menu", function()
     end
         end
 end )
-hook.Add ("Think", "Close Menu", function ()
-    if( input.IsKeyDown( KEY_DELETE ) ) then
-        key_ran = 1
-    end
-end)
         local function HackDerma()
         local midW, midH = ScrW() / 2
-        local HackDerma = vgui.Create("DFrame")
+        HackDerma = vgui.Create("DFrame")
         HackDerma:SetSize(500, 450)
         HackDerma:Center()
         HackDerma:SetTitle( "Hack Menu" )
+        HackDerma:ShowCloseButton(false)
+    
+        myButton = vgui.Create("DButton", HackDerma)
+        myButton:SetText( "Close" )
+        myButton:SetPos( 458, 4)
+        myButton:SetSize(90, 28)
+        myButton:SizeToContents()
+        myButton.DoClick = function() HackDerma:Close() key_ran = 1 end
         HackDerma:MakePopup()
+
+        local ESPenable = vgui.Create("DButton", HackDerma)
+        ESPenable:SetPos(40, 50)
+        ESPenable:SetSize(90, 28)
+        ESPenable:SetText("Enable ESP")
+        function  ESPenable:OnMousePressed()
+      print("ESP activated")
+      end
       end
       concommand.Add( "open", HackDerma )
 end
